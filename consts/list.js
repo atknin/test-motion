eb0d4145d5530d6f9bf77396c60942109569f4fa
atknini <atknini@yahoo.com>
2021-05-18 01:34:11 +0300

    e

---
+++ consts/list.js
@@ -0,0 +1,68 @@
+const places = [
+  {
+    id: '1',
+    name: 'Настя Фадина студия MyRide',
+    location: 'Соул Сайкл, 45 мин',
+    image: require('../assets/video/1.png'),
+    uri: 'https://homecycle.online/hcvideo/Qgl9TDax-pCedmX8t.mp4',
+    details: `Соул сайкл тренировка, 45 мин`,
+  },
+  {
+    id: '2',
+    name: 'Олег Лысенко студия MyRide, MCF',
+    location: 'Велогонка, 55 мин',
+    image: require('../assets/video/2.png'),
+    uri: 'https://homecycle.online/hcvideo/CMd00UPm-pCedmX8t.mp4',
+    details: `Тренировка в формате велогонки, 55 минут`,
+  },
+  {
+    id: '3',
+    name: 'Олег Лысенко студия MyRide, MCF',
+    location: 'Велогонка, 55 мин',
+    image: require('../assets/video/3.png'),
+    uri: 'https://homecycle.online/hcvideo/mGGOMLu2-pCedmX8t.mp4',
+    details: `Тренировка в формате велогонки, 55 минут`,
+  },
+  {
+    id: '4',
+    name: 'Настя Фадина студия MyRide',
+    location: 'Соул сайкл, 50 мин',
+    image: require('../assets/video/4.png'),
+    uri: 'https://homecycle.online/hcvideo/sj0UUc8H-pCedmX8t.mp4',
+    details: `Соул сайкл тренировка, 50 мин`,
+  },
+  {
+    id: '5',
+    name: 'Настя Фадина студия MyRide',
+    location: 'Соул сайкл, 40 мин',
+    image: require('../assets/video/5.png'),
+    uri: 'https://homecycle.online/hcvideo/E9SemkLH-pCedmX8t.mp4',
+    details: `Соул сайкл тренировка, 40 мин`,
+  },
+  {аваа
+    id: '6',
+    name: 'Настя Фадина студия MyRide',
+    location: 'Соул сайкл, 48 мин',
+    image: require('../assets/video/6.png'),
+    uri: 'https://homecycle.online/hcvideo/LD61SX8C-pCedmX8t.mp4',
+    details: `Соул сайкл тренировка, 48 мин`,
+  },{
+    id: '6',
+    name: 'Настя Фадина студия MyRide',
+    location: 'Соул сайкл, 48 мин',
+    image: require('../assets/video/6.png'),
+    uri: 'https://homecycle.online/hcvideo/LD61SX8C-pCedmX8t.mp4',
+    details: `Соул сайкл тренировка, 48 мин`,
+  },{
+    id: '24',
+    name: 'Олег Лысенко студия MyRide, MCF',
+    location: 'Велогонка, 55 мин',
+    image: require('../assets/video/24.png'),
+    uri: 'https://homecycle.online/hcvideo/A3EZKGTk-pCedmX8t.mp4',
+    details: `Тренировка в формате велогонки, 55 минут`,
+  }
+
+
+];
+
+export default places;
